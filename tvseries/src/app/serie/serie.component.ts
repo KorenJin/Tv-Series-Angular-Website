3e5e00f0a2a5a19c36ad82e9f6725e464b38c44c
Ronald Pardo <r.diazp@uniandes.edu.co>
2022-10-31 01:50:46 -0500

    Angular JS workshop: ISIS 2603

---
+++ tvseries/src/app/serie/serie.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { defaultseries } from './dataSeries';
+import { SerieService } from './serie.service';
+
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+
+  series: Serie[] = [];
+  average: number = 0;
+  caption: string = "";
+
+  constructor(private serieService: SerieService) { }
+
+  ngOnInit() {
+    this.getSeries();
+  }
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(
+        series => {
+          this.series = series;
+          this.setAverage(series);
+        }
+      );
+  }
+
+  setAverage(series: Serie[] = this.series) {
+    // Get the average of the series seasons
+    this.average = series.reduce((sum, serie) => sum + serie.seasons, 0) / series.length;
+    this.caption = "Seasons average: " + this.average;
+  }
+
+}
